40d2d31c7207d1e3a91999da0a5e10a87c1915cb
Steffen <steffen.schotthoefer@kit.edu>
2022-02-01 15:11:12 +0100

    start transformer

--- model.py
+++ model.py
@@ -25,3 +25,27 @@ class LoadForecaster(nn.Module):
         cell_state = torch.randn(self.num_layers, batch_size, self.hidden_size, device=self.device)
 
         return hidden_state, cell_state
+
+
+class LoadForecasterTransformer(nn.Module):
+    def __init__(self, input_size: int, hidden_size: int, output_size: int,
+                 num_layer: int = 1, dropout: float = 0, batch_first: bool = True, device: torch.device = None):
+        super(LoadForecasterTransformer, self).__init__()
+
+        self.hidden_size = hidden_size
+        self.num_layers = num_layer
+        self.device = device
+
+        self.lstm = nn.Transformer(nhead=16, num_encoder_layers=12)
+        self.fully_connected = nn.Linear(hidden_size, output_size, device=self.device)
+
+    def forward(self, input_sequence, hidden):
+        output, hidden = self.lstm(input_sequence, hidden)
+        output = self.fully_connected(output)
+        return output, hidden
+
+    def init_hidden(self, batch_size):
+        hidden_state = torch.zeros(self.num_layers, batch_size, self.hidden_size, device=self.device)
+        cell_state = torch.randn(self.num_layers, batch_size, self.hidden_size, device=self.device)
+
+        return hidden_state, cell_state
